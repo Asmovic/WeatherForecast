77785c14c7a1429c7315123d0bfd32590b58fc3e
Asmovic <Asogba007@gmail.com>
2019-05-17 10:52:28 +0100

    my fv commit

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express'),
+    cors = require('cors'),
     path = require('path'),
     PORT = process.env.PORT || 8080,
     app = express();
-
+app.use(cors);
 app.use(express.static(__dirname));
 
 app.get('*', (req, res) => {
