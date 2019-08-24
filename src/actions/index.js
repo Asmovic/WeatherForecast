30169949824a5d8390de20791cfe091b62081ded
asmovic <owoyemifood@gmail.com>
2019-08-24 22:18:01 +0100

    map resolve

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
-const API_KEY = 'ffb2f0b85301c8b7c7039a40843cc5c7';
+/* const API_KEY = 'ffb2f0b85301c8b7c7039a40843cc5c7'; */
+const API_KEY = "8fecdddd262f91a026ccee6d22c1827b";
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export function fetchWeather(city) {
-    const url = `${ROOT_URL}&q=${city},us`;
+    const url = `${ROOT_URL}&q=${city},NG`;
     const request = axios.get(url);
 
     return {
--- src/components/app.js
+++ src/components/app.js
@@ -6,9 +6,21 @@ export default class App extends Component {
     render() {
         return (
             <div>
+                <h1 style={styles.headerStyle}>Locate any City in Nigeria</h1>
                 <SearchBar />
                 <WeatherList />
             </div>
         );
+    }
+}
+
+const styles = {
+    headerStyle: {
+        flex: 1,
+        fontSize: 28,
+        textAlign: 'center',
+        marginTop: 15,
+        fontWeight: 'bold'
+
     }
 }
